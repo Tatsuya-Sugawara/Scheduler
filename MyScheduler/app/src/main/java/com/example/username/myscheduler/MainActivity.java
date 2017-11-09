ed3fd7568557ba41520646a62db9dfcc83ba732a
Fumiya Takahashi <fumiya.takahashi.sys@gmail.com>
2017-11-09 14:07:09 +0900

    haikei tuika

--- MyScheduler/app/src/main/java/com/example/username/myscheduler/MainActivity.java
+++ MyScheduler/app/src/main/java/com/example/username/myscheduler/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.username.myscheduler;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
