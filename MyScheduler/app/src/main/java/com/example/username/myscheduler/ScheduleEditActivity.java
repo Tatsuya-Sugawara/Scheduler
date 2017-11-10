983823bfdd0acc150ae942a82cf14c5d3826a084
MichiyaMuramatsu <michiya.muramatsu.sys@gmail.com>
2017-11-10 10:05:10 +0900

    OCR

--- MyScheduler/app/src/main/java/com/example/username/myscheduler/ScheduleEditActivity.java
+++ MyScheduler/app/src/main/java/com/example/username/myscheduler/ScheduleEditActivity.java
@@ -1,14 +1,31 @@
 package com.example.username.myscheduler;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.googlecode.tesseract.android.TessBaseAPI;
+
+import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -16,7 +33,17 @@ import java.util.Date;
 import io.realm.Realm;
 import io.realm.RealmResults;
 
+import static android.content.ContentValues.TAG;
+
 public class ScheduleEditActivity extends AppCompatActivity {
+
+    static final int PHOTO_REQUEST_CODE = 1;
+    private TessBaseAPI tessBaseApi;
+    private static final String lang = "eng";
+    private String DATA_PATH;
+    private static final String TESSDATA = "tessdata";
+
+    private TextView textView;
 
     private Realm mRealm;
     EditText mDateEdit;
@@ -28,6 +55,35 @@ public class ScheduleEditActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule_edit);
+
+        DATA_PATH = getFilesDir().toString() + "/OCR/";
+
+        textView = (TextView) findViewById(R.id.textResult);
+
+        Log.v("てすと",DATA_PATH);
+        Log.v("てすと",getFilesDir().toString());
+
+        prepareDirectory(DATA_PATH);
+        prepareDirectory(DATA_PATH + TESSDATA);
+        copyTessDataFiles(TESSDATA);
+
+
+        Button captureImg = (Button) findViewById(R.id.action_btn);
+        if (captureImg != null) {
+            captureImg.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(Build.VERSION.SDK_INT > 19) {
+                        Log.v("てすと","化石じゃないです");
+                        selectGallery();
+                    } else {
+                        Log.v("てすと","化石です");
+                        selectGalleryNamekuzi();
+                    }
+                }
+            });
+        }
+
         mRealm = Realm.getDefaultInstance();
         mDateEdit = (EditText) findViewById(R.id.dateEdit);
         mTitleEdit = (EditText) findViewById(R.id.titleEdit);
@@ -49,6 +105,121 @@ public class ScheduleEditActivity extends AppCompatActivity {
             mDelete.setVisibility(View.INVISIBLE);
         }
     }
+
+    private void selectGalleryNamekuzi() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, PHOTO_REQUEST_CODE);
+    }
+
+    private void selectGallery() {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("image/*");
+        startActivityForResult(intent, PHOTO_REQUEST_CODE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode,
+                                 Intent data) {
+        if (requestCode == PHOTO_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            Uri uri = null;
+            Bitmap bitmap = null;
+            if(data != null) {
+                uri = data.getData();
+                try {
+                    bitmap = getBitmapFromUri(uri);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            textView.setText(extractText(bitmap));
+
+        } else {
+            Toast.makeText(this, "ERROR: Image was not obtained.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void prepareDirectory(String path) {
+
+        File dir = new File(path);
+        if (!dir.exists()) {
+           // Log.v("てすと",path + "にはファイルはありません");
+            if (dir.mkdirs()) {
+               // Log.v("てすと",path + "にファイル作成されました");
+            } else {
+              //  Log.v("てすと",path + "にはファイル作成されませんでした");
+            }
+        } else {
+          //  Log.v("てすと",path + "にはファイルは存在します");
+        }
+    }
+
+    private void copyTessDataFiles(String path) {
+        try {
+            String fileList[] = getAssets().list(path);
+
+            for (String fileName : fileList) {
+
+                String pathToDataFile = DATA_PATH + path + "/" + fileName;
+
+                Log.v("てすと",DATA_PATH + path + "/" + fileName);
+
+                if (!(new File(pathToDataFile)).exists()) {
+
+                    InputStream in = getAssets().open(path + "/" + fileName);
+
+                    OutputStream out = new FileOutputStream(pathToDataFile);
+
+                    byte[] buf = new byte[1024];
+                    int len;
+
+                    while ((len = in.read(buf)) > 0) {
+                        out.write(buf, 0, len);
+                    }
+                    in.close();
+                    out.close();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private String extractText(Bitmap bitmap) {
+        try {
+            tessBaseApi = new TessBaseAPI();
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage());
+            if (tessBaseApi == null) {
+                Log.e(TAG, "TessBaseAPI is null. TessFactory not returning tess object.");
+            }
+        }
+
+        tessBaseApi.init(DATA_PATH, lang);
+
+        Log.d(TAG, "Training file loaded");
+        tessBaseApi.setImage(bitmap);
+        String extractedText = "empty result";
+        try {
+            extractedText = tessBaseApi.getUTF8Text();
+        } catch (Exception e) {
+            Log.e(TAG, "Error in recognizing text.");
+        }
+        tessBaseApi.end();
+        return extractedText;
+    }
+
+    private Bitmap getBitmapFromUri(Uri uri) throws IOException {
+        ParcelFileDescriptor parcelFileDescriptor =
+                getContentResolver().openFileDescriptor(uri, "r");
+        FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+        Bitmap image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+        parcelFileDescriptor.close();
+        return image;
+    }
+
 
     public void onSaveTapped(View view) {
 
