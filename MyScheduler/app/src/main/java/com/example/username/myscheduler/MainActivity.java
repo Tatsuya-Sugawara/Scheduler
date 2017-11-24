2eedbe4f9b99015b71f74c9cfdce168338d62f64
Tatsuya Sugawara <tatsuya.sugawara.sys@gmail.com>
2017-11-24 10:06:57 +0900

    add comment

--- MyScheduler/app/src/main/java/com/example/username/myscheduler/MainActivity.java
+++ MyScheduler/app/src/main/java/com/example/username/myscheduler/MainActivity.java
@@ -5,22 +5,15 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.app.NotificationCompat;
-import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.CalendarView;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import java.util.Date;
 
@@ -35,10 +28,8 @@ public class MainActivity extends AppCompatActivity {
     private CalendarView calendarView;
     private RealmResults<Schedule> schedules;
     ScheduleAdapter adapter;
-    FloatingActionButton fab;
-    public  static  String onDate;
-    public  static String sEAyear;
-    private final static int NOTIFICATION_MINIMUM_ID = 0;
+    int cnt;
+    private static final int NOTIFICATION_MINIMUM_ID = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,21 +43,11 @@ public class MainActivity extends AppCompatActivity {
         }
 
         setContentView(R.layout.activity_main);
-
-        fab = (FloatingActionButton) findViewById(R.id.fab);
-
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
-
             @Override
-            public void onClick(View view) {
-                if(view == fab){
-                    Toast toast = Toast.makeText(MainActivity.this, "日付を選択してください", Toast.LENGTH_SHORT);
-                    toast.show();
-                }
-
-            }
+            public void onClick(View view) {startActivity(new Intent(MainActivity.this,ScheduleEditActivity.class));}
         });
-
         mR = Realm.getDefaultInstance();
 
         mLV = (ListView) findViewById(R.id.listView);
@@ -76,16 +57,7 @@ public class MainActivity extends AppCompatActivity {
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener(){
 
             public  void onSelectedDayChange(CalendarView view, int year,int month,int dayOfMonth){
-
-                onDate = (year + "/" + (month + 1) +  "/" + dayOfMonth);
-                sEAyear = String.valueOf(year);
-                fab.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {startActivity(new Intent(MainActivity.this,ScheduleEditActivity.class));}
-                });
-
                 Date date = new Date(year-1900,month,dayOfMonth);
-
 
                 date.setHours(00);
                 date.setMinutes(00);
@@ -119,30 +91,25 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
 
-        Bitmap largeIcon = BitmapFactory.decodeResource(getResources(), R.drawable.aicon);
-        NotificationManager nm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE); //Notificationのインポートはv4で
+
         Intent intent = new Intent(this, MainActivity.class);
         intent.putExtra("PARAM", 1);
         PendingIntent penintent = PendingIntent.getActivity(this,
                 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-        Notification builder = new NotificationCompat.Builder(this)                  //thisとgetApplicationContext()との違いが判らないのでコメ残す
-                .setContentTitle("もやし")                                     //  タイトル（太字）
-                .setContentText("タップしてアプリを起動します")                //  メッセージテキスト
-                .setPriority(Integer.MAX_VALUE)                                  //通知を一番上に表示する
-                .setAutoCancel(false)                                             //タップされた時通知バーから消去する場合はtrue
-                .setSmallIcon(R.drawable.aicon)                                   //左側のアイコン画像
-                // .setLargeIcon(largeIcon)                                      　  //右側にアイコン画像表示される、お好みで
-                .setContentIntent(penintent)                                       //タップされた時の振舞い
-                .build();                                                          //通知の作成
-        Notification notification = new Notification();
+
+        NotificationManager nm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        Notification notification = new Notification.Builder(this)
+                .setContentTitle("もやし")    //  タイトル
+                .setContentText("タップしてアプリを起動します")    //  メッセージ
+                .setContentIntent(penintent)    //  タップされた時の動作
+                .setAutoCancel(false)    //  タップしたときに通知バーから消去する場合はtrue
+                .setSmallIcon(R.drawable.aicon)    //  左側のアイコン画像
+                .build();
+
         notification.flags = Notification.FLAG_NO_CLEAR;
-        NotificationManagerCompat manager = NotificationManagerCompat.from(getApplicationContext());
-        manager.notify(NOTIFICATION_MINIMUM_ID, builder);
 
-
-
-
-
+        nm.notify(NOTIFICATION_MINIMUM_ID, notification);    //引数は適当に作っちゃって、どうぞ
     }
 
 
--- MyScheduler/app/src/main/java/com/example/username/myscheduler/ScheduleEditActivity.java
+++ MyScheduler/app/src/main/java/com/example/username/myscheduler/ScheduleEditActivity.java
@@ -1,32 +1,12 @@
 package com.example.username.myscheduler;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Color;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.ParcelFileDescriptor;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-//import android.support.v7.util.ThreadUtil;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.googlecode.tesseract.android.TessBaseAPI;
-
-import java.io.File;
-import java.io.FileDescriptor;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -34,19 +14,9 @@ import java.util.Date;
 import io.realm.Realm;
 import io.realm.RealmResults;
 
-import static android.content.ContentValues.TAG;
-
 public class ScheduleEditActivity extends AppCompatActivity {
 
-    static final int PHOTO_REQUEST_CODE = 1;
-    private TessBaseAPI tessBaseApi;
-    private static final String lang = "jpn";
-    private String DATA_PATH;
-    private static final String TESSDATA = "tessdata";
-
-
     private Realm mRealm;
-
     EditText mDateEdit;
     EditText mTitleEdit;
     EditText mDetailEdit;
@@ -56,47 +26,14 @@ public class ScheduleEditActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule_edit);
-
-        DATA_PATH = getFilesDir().toString() + "/OCR/";
-
-       // textView = (TextView) findViewById(R.id.textResult);
-
-        Log.v("てすと",DATA_PATH);
-        Log.v("てすと",getFilesDir().toString());
-
-        prepareDirectory(DATA_PATH);
-        prepareDirectory(DATA_PATH + TESSDATA);
-        copyTessDataFiles(TESSDATA);
-
-
-        Button captureImg = (Button) findViewById(R.id.action_btn);
-        if (captureImg != null) {
-            captureImg.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if(Build.VERSION.SDK_INT > 19) {
-                        Log.v("てすと","化石じゃないです");
-                        selectGallery();
-                    } else {
-                        Log.v("てすと","化石です");
-                        selectGalleryNamekuzi();
-                    }
-                }
-            });
-        }
-
         mRealm = Realm.getDefaultInstance();
         mDateEdit = (EditText) findViewById(R.id.dateEdit);
-
         mTitleEdit = (EditText) findViewById(R.id.titleEdit);
         mDetailEdit = (EditText) findViewById(R.id.detailEdit);
         mDelete = (Button) findViewById(R.id.delete);
 
-
-
         long scheduleId = getIntent().getLongExtra("schedule_id", -1);
         if (scheduleId != -1) {
-//            System.out.println("編集画面!!");
             RealmResults<Schedule> results = mRealm.where(Schedule.class)
                     .equalTo("id", scheduleId).findAll();
             Schedule schedule = results.first();
@@ -107,129 +44,9 @@ public class ScheduleEditActivity extends AppCompatActivity {
             mDetailEdit.setText(schedule.getDetail());
             mDelete.setVisibility(View.VISIBLE);
         } else {
-//            System.out.println("初期登録!!");
-            mDateEdit.setText(MainActivity.onDate);
             mDelete.setVisibility(View.INVISIBLE);
         }
     }
-
-    private void selectGalleryNamekuzi() {
-        Intent intent = new Intent(Intent.ACTION_PICK);
-        intent.setType("image/*");
-        startActivityForResult(intent, PHOTO_REQUEST_CODE);
-    }
-
-    private void selectGallery() {
-        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
-        intent.addCategory(Intent.CATEGORY_OPENABLE);
-        intent.setType("image/*");
-        startActivityForResult(intent, PHOTO_REQUEST_CODE);
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode,
-                                 Intent data) {
-        if (requestCode == PHOTO_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
-            Uri uri = null;
-            Bitmap bitmap = null;
-            if(data != null) {
-                uri = data.getData();
-                try {
-                    bitmap = getBitmapFromUri(uri);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            mDetailEdit.setText(extractText(bitmap));
-            Match match = new Match();
-
-            mDateEdit.setText(match.isMatch(extractText(bitmap),MainActivity.sEAyear,MainActivity.onDate));
-
-        } else {
-            Toast.makeText(this, "ERROR: Image was not obtained.", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    private void prepareDirectory(String path) {
-
-        File dir = new File(path);
-        if (!dir.exists()) {
-           // Log.v("てすと",path + "にはファイルはありません");
-            if (dir.mkdirs()) {
-               // Log.v("てすと",path + "にファイル作成されました");
-            } else {
-              //  Log.v("てすと",path + "にはファイル作成されませんでした");
-            }
-        } else {
-          //  Log.v("てすと",path + "にはファイルは存在します");
-        }
-    }
-
-    private void copyTessDataFiles(String path) {
-        try {
-            String fileList[] = getAssets().list(path);
-
-            for (String fileName : fileList) {
-
-                String pathToDataFile = DATA_PATH + path + "/" + fileName;
-
-                Log.v("てすと",DATA_PATH + path + "/" + fileName);
-
-                if (!(new File(pathToDataFile)).exists()) {
-
-                    InputStream in = getAssets().open(path + "/" + fileName);
-
-                    OutputStream out = new FileOutputStream(pathToDataFile);
-
-                    byte[] buf = new byte[1024];
-                    int len;
-
-                    while ((len = in.read(buf)) > 0) {
-                        out.write(buf, 0, len);
-                    }
-                    in.close();
-                    out.close();
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-
-    private String extractText(Bitmap bitmap) {
-        try {
-            tessBaseApi = new TessBaseAPI();
-        } catch (Exception e) {
-            Log.e(TAG, e.getMessage());
-            if (tessBaseApi == null) {
-                Log.e(TAG, "TessBaseAPI is null. TessFactory not returning tess object.");
-            }
-        }
-
-        tessBaseApi.init(DATA_PATH, lang);
-
-        Log.d(TAG, "Training file loaded");
-        tessBaseApi.setImage(bitmap);
-        String extractedText = "empty result";
-        try {
-            extractedText = tessBaseApi.getUTF8Text();
-        } catch (Exception e) {
-            Log.e(TAG, "Error in recognizing text.");
-        }
-        tessBaseApi.end();
-        return extractedText;
-    }
-
-    private Bitmap getBitmapFromUri(Uri uri) throws IOException {
-        ParcelFileDescriptor parcelFileDescriptor =
-                getContentResolver().openFileDescriptor(uri, "r");
-        FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
-        Bitmap image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
-        parcelFileDescriptor.close();
-        return image;
-    }
-
 
     public void onSaveTapped(View view) {
 
@@ -260,16 +77,8 @@ public class ScheduleEditActivity extends AppCompatActivity {
                     schedule.setDetail(mDetailEdit.getText().toString());
                 }
             });
-            Snackbar.make(findViewById(android.R.id.content),
-                    "アップデートしました", Snackbar.LENGTH_LONG)
-                    .setAction("戻る", new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            finish();
-                        }
-                    })
-                    .setActionTextColor(Color.YELLOW)
-                    .show();
+            Toast.makeText(this, "更新しました", Toast.LENGTH_SHORT).show();
+            finish();
         } else {
             mRealm.executeTransaction(new Realm.Transaction() {
                 @Override
@@ -289,7 +98,7 @@ public class ScheduleEditActivity extends AppCompatActivity {
             finish();
         }
 
-
+        System.out.println(date);
     }
 
     public void onDeleteTapped(View view) {
