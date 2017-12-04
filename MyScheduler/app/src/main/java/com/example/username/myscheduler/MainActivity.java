29e9d7cff2da15e61100c32787b344ae4fbf8806
Fumiya Takahashi <fumiya.takahashi.sys@gmail.com>
2017-12-04 11:10:24 +0900

    更新

---
+++ MyScheduler/app/src/main/java/com/example/username/myscheduler/MainActivity.java
@@ -0,0 +1,138 @@
+package com.example.username.myscheduler;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.CalendarView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.Date;
+
+import io.realm.Realm;
+import io.realm.RealmQuery;
+import io.realm.RealmResults;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Realm mR;
+    private ListView mLV;
+    private CalendarView calendarView;
+    private RealmResults<Schedule> schedules;
+    ScheduleAdapter adapter;
+    FloatingActionButton fab;
+    public  static  String onDate;
+    public  static String sEAyear;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (Build.VERSION.SDK_INT >= 19) {
+            View decor = this.getWindow().getDecorView();
+            decor.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        } else {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        }
+
+        setContentView(R.layout.activity_main);
+
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                if(view == fab){
+                    Toast toast = Toast.makeText(MainActivity.this, "日付を選択してください", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+
+            }
+        });
+
+        mR = Realm.getDefaultInstance();
+
+        mLV = (ListView) findViewById(R.id.listView);
+
+        calendarView = (CalendarView) findViewById(R.id.calendarView);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener(){
+
+            public  void onSelectedDayChange(CalendarView view, int year,int month,int dayOfMonth){
+
+                onDate = (year + "/" + (month + 1) +  "/" + dayOfMonth);
+                sEAyear = String.valueOf(year);
+                fab.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {startActivity(new Intent(MainActivity.this,ScheduleEditActivity.class));}
+                });
+
+                Date date = new Date(year-1900,month,dayOfMonth);
+
+
+                date.setHours(00);
+                date.setMinutes(00);
+                date.setSeconds(00);
+
+
+                RealmQuery<Schedule> rQ = mR.where(Schedule.class);
+
+
+                System.out.println("scheduleDBの中身" + mR.where(Schedule.class).findAll());
+
+                rQ.equalTo("date",date);
+
+                System.out.println("scheduleDBの検索結果" + rQ.findAll());
+                schedules = rQ.findAll();
+
+
+                adapter = new ScheduleAdapter(schedules,calendarView);
+                mLV.setAdapter(adapter);
+            }
+        });
+
+
+        mLV.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view,
+                                            int position, long id) {
+                        Schedule schedule = (Schedule) parent.getItemAtPosition(position);
+                        startActivity(new Intent(MainActivity.this,ScheduleEditActivity.class).putExtra("schedule_id", schedule.getId()));
+                    }
+                });
+
+        Intent intent = new Intent(this, MainActivity.class/*ここで指定したクラスが通知をタップした時に呼び出される*/);
+        intent.putExtra("PARAM", 1);
+        PendingIntent penintent = PendingIntent.getActivity(this,
+                0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        Notification notification = new Notification.Builder(this)
+                .setContentTitle("もやし")    //  タイトル
+                .setContentText("タップしてアプリを起動します")    //  メッセージ
+                .setContentIntent(penintent)    //  タップされた時の動作
+                .setAutoCancel(false)    //  タップしたときに通知バーから消去する場合はtrue
+                .setSmallIcon(R.drawable.aicon)   //  左側のアイコン画像
+                .build();                       //通知の作成
+        notification.flags = Notification.FLAG_NO_CLEAR;  //通知が本来ならこのコードで固定されるっぽいが動作しない
+        notification.flags = Notification.FLAG_ONGOING_EVENT; //こっちも無理
+
+        NotificationManager nm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        nm.notify(1, notification);    //引数は適当に
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mR.close();
+    }
+}
---
+++ MyScheduler/app/src/main/java/com/example/username/myscheduler/ScheduleEditActivity.java
@@ -0,0 +1,307 @@
+package com.example.username.myscheduler;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.googlecode.tesseract.android.TessBaseAPI;
+
+import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import io.realm.Realm;
+import io.realm.RealmResults;
+
+import static android.content.ContentValues.TAG;
+
+//import android.support.v7.util.ThreadUtil;
+
+public class ScheduleEditActivity extends AppCompatActivity {
+
+    static final int PHOTO_REQUEST_CODE = 1;
+    private TessBaseAPI tessBaseApi;
+    private static final String lang = "jpn";
+    private String DATA_PATH;
+    private static final String TESSDATA = "tessdata";
+    private Realm mRealm;
+    EditText mDateEdit;
+    EditText mTitleEdit;
+    EditText mDetailEdit;
+    Button mDelete;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_schedule_edit);
+
+        DATA_PATH = getFilesDir().toString() + "/OCR/";
+        // textView = (TextView) findViewById(R.id.textResult);
+        //Log.v("てすと",DATA_PATH);
+        //Log.v("てすと",getFilesDir().toString());
+        prepareDirectory(DATA_PATH);
+        prepareDirectory(DATA_PATH + TESSDATA);
+        copyTessDataFiles(TESSDATA);
+
+        Button captureImg = (Button) findViewById(R.id.action_btn);
+        if (captureImg != null) {
+            captureImg.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(Build.VERSION.SDK_INT > 19) {
+                        Log.v("てすと","化石じゃないです");
+                        selectGallery();
+                    } else {
+                        Log.v("てすと","化石です");
+                        selectGalleryNamekuzi();
+                    }
+                }
+            });
+        }
+
+        mRealm = Realm.getDefaultInstance();
+        mDateEdit = (EditText) findViewById(R.id.dateEdit);
+        mTitleEdit = (EditText) findViewById(R.id.titleEdit);
+        mDetailEdit = (EditText) findViewById(R.id.detailEdit);
+        mDelete = (Button) findViewById(R.id.delete);
+
+
+        long scheduleId = getIntent().getLongExtra("schedule_id", -1);
+        if (scheduleId != -1) {
+//編集画面
+            RealmResults<Schedule> results = mRealm.where(Schedule.class)
+                    .equalTo("id", scheduleId).findAll();
+            Schedule schedule = results.first();
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+            String date = sdf.format(schedule.getDate());
+            mDateEdit.setText(date);
+            mTitleEdit.setText(schedule.getTitle());
+            mDetailEdit.setText(schedule.getDetail());
+
+            mDelete.setVisibility(View.VISIBLE);
+        } else {
+//初期登録
+            mDateEdit.setText(MainActivity.onDate);
+            mDelete.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    private void selectGalleryNamekuzi() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, PHOTO_REQUEST_CODE);
+    }
+
+    private void selectGallery() {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("image/*");
+        startActivityForResult(intent, PHOTO_REQUEST_CODE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode,
+                                 Intent data) {
+        if (requestCode == PHOTO_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            Uri uri = null;
+            Bitmap bitmap = null;
+            if(data != null) {
+                uri = data.getData();
+                try {
+                    bitmap = getBitmapFromUri(uri);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+<<<<<<< HEAD
+            mDetailEdit.setText("");
+=======
+            mDetailEdit.setText(extractText(bitmap));
+>>>>>>> 2d4a26127e400a01297435264c0ca6cebdae07c9
+            Match match = new Match();
+
+            mDateEdit.setText(match.isMatch(extractText(bitmap),MainActivity.sEAyear,MainActivity.onDate));
+            if(match.isMatch(extractText(bitmap),MainActivity.sEAyear,MainActivity.onDate).equals(MainActivity.onDate)){
+                Toast.makeText(this, "日付が読み込めませんでした。\n選択日の日付を入力します。", Toast.LENGTH_LONG).show();
+            }
+
+        } else {
+            Toast.makeText(this, "ERROR: Image was not obtained.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void prepareDirectory(String path) {
+
+        File dir = new File(path);
+        if (!dir.exists()) {
+           // Log.v("てすと",path + "にはファイルはありません");
+            if (dir.mkdirs()) {
+               // Log.v("てすと",path + "にファイル作成されました");
+            } else {
+              //  Log.v("てすと",path + "にはファイル作成されませんでした");
+            }
+        } else {
+          //  Log.v("てすと",path + "にはファイルは存在します");
+        }
+    }
+
+    private void copyTessDataFiles(String path) {
+        try {
+            String fileList[] = getAssets().list(path);
+
+            for (String fileName : fileList) {
+
+                String pathToDataFile = DATA_PATH + path + "/" + fileName;
+
+                Log.v("てすと",DATA_PATH + path + "/" + fileName);
+
+                if (!(new File(pathToDataFile)).exists()) {
+
+                    InputStream in = getAssets().open(path + "/" + fileName);
+
+                    OutputStream out = new FileOutputStream(pathToDataFile);
+
+                    byte[] buf = new byte[1024];
+                    int len;
+
+                    while ((len = in.read(buf)) > 0) {
+                        out.write(buf, 0, len);
+                    }
+                    in.close();
+                    out.close();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private String extractText(Bitmap bitmap) {
+        try {
+            tessBaseApi = new TessBaseAPI();
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage());
+            if (tessBaseApi == null) {
+                Log.e(TAG, "TessBaseAPI is null. TessFactory not returning tess object.");
+            }
+        }
+
+        tessBaseApi.init(DATA_PATH, lang);
+
+        Log.d(TAG, "Training file loaded");
+        tessBaseApi.setImage(bitmap);
+        String extractedText = "empty result";
+        try {
+            extractedText = tessBaseApi.getUTF8Text();
+        } catch (Exception e) {
+            Log.e(TAG, "Error in recognizing text.");
+        }
+        tessBaseApi.end();
+        return extractedText;
+    }
+
+    private Bitmap getBitmapFromUri(Uri uri) throws IOException {
+        ParcelFileDescriptor parcelFileDescriptor =
+                getContentResolver().openFileDescriptor(uri, "r");
+        FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+        Bitmap image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+        parcelFileDescriptor.close();
+        return image;
+    }
+
+
+    public void onSaveTapped(View view) {
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+        Date dateParse = new Date();
+        try {
+            dateParse = sdf.parse(mDateEdit.getText().toString());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        Date workDate  = new Date(dateParse.getYear(),dateParse.getMonth(),dateParse.getDate());
+        workDate.setHours(00);
+        workDate.setMinutes(00);
+        workDate.setSeconds(00);
+
+        final Date date = workDate;
+        long scheduleId = getIntent().getLongExtra("schedule_id", -1);
+        if (scheduleId != -1) {
+            final RealmResults<Schedule> results = mRealm.where(Schedule.class)
+                    .equalTo("id", scheduleId).findAll();
+            mRealm.executeTransaction(new Realm.Transaction() {
+                @Override
+                public void execute(Realm realm) {
+                    Schedule schedule = results.first();
+                    schedule.setDate(date);
+                    schedule.setTitle(mTitleEdit.getText().toString());
+                    schedule.setDetail(mDetailEdit.getText().toString());
+                }
+            });
+            Toast.makeText(this, "更新しました", Toast.LENGTH_SHORT).show();
+            finish();
+        } else {
+            mRealm.executeTransaction(new Realm.Transaction() {
+                @Override
+                public void execute(Realm realm) {
+                    Number maxId = realm.where(Schedule.class).max("id");
+                    long nextId = 0;
+                    if (maxId != null) nextId = maxId.longValue() + 1;
+                    Schedule schedule
+                            = realm.createObject(Schedule.class, new Long(nextId));
+                    schedule.setDate(date);
+                    schedule.setTitle(mTitleEdit.getText().toString());
+                    schedule.setDetail(mDetailEdit.getText().toString());
+                }
+            });
+
+            Toast.makeText(this, "追加しました", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+    }
+
+    public void onDeleteTapped(View view) {
+        final long scheduleId = getIntent().getLongExtra("schedule_id", -1);
+        if (scheduleId != -1) {
+            mRealm.executeTransaction(new Realm.Transaction() {
+                @Override
+                public void execute(Realm realm) {
+                    Schedule schedule = realm.where(Schedule.class)
+                            .equalTo("id", scheduleId).findFirst();
+                    schedule.deleteFromRealm();
+                }
+            });
+            Toast.makeText(this, "削除しました", Toast.LENGTH_LONG).show();
+            finish();
+        }
+    }
+
+    public void onBackTapped(View view){
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mRealm.close();
+    }
+}
